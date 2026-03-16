e8d4974c0e8370c72cdcbc2c9a6f2ffd843857d5
Caio Freitas <caio078freitas@gmail.com>
2026-03-16 14:54:02 -0300

    chore(release): update version to 0.2.1

--- api/main.py
+++ api/main.py
@@ -54,7 +54,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="InsectAPI",
     description="Read-Only API for CTFB Insecta taxonomic data.",
-    version="0.2.0",
+    version="0.2.1",
     lifespan=lifespan,
 )
 
